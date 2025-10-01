a795e6e4c9ea2ac7884aa913efe973557fc8ba10
lerrel129 <45817340+lerrel129@users.noreply.github.com>
2025-10-01 19:33:16 +0200

    Update hellspy.js

--- hellspy.js
+++ hellspy.js
@@ -155,8 +155,7 @@ export default (req, res) => {
     return serveHTTP(builder.getInterface())(req, res);}
 
 console.log("test rqeuest");
-url = `https://api.hellspy.to/gw/search?query=${query.replaceAll(" ", "%20")}&offset=0&limit=99`
-// url = `https://api.hellspy.to/gw/search?query=problem child 2&offset=0&limit=99`
+url = `https://api.hellspy.to/gw/search?query=problem child 2&offset=0&limit=99`
 // const response = await axios.get(url, {
 // 	  headers: {
 // 		"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:120.0) Gecko/20100101 Firefox/120.0",
@@ -172,6 +171,7 @@ url = `https://api.hellspy.to/gw/search?query=${query.replaceAll(" ", "%20")}&of
 // else
 // 	console.log(responseerror , response.status)
 // 	console.log("test rqeuest false")
+
 
 
 

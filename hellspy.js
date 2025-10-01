020208b9b30fafecf0edb8dd158e5aee191884ea
lerrel129 <45817340+lerrel129@users.noreply.github.com>
2025-10-01 12:58:19 +0200

    Add files via upload

---
+++ hellspy.js
@@ -0,0 +1,151 @@
+const { addonBuilder, serveHTTP, publishToCentral }  = require('stremio-addon-sdk')
+const axios = require("axios")
+
+const builder = new addonBuilder({
+    id: 'Hellspy',
+    version: '1.0.0',
+    name: 'Hellspy.to',
+    description: "Hellspy video streaming",
+    logo: "https://www.hellspy.to/img/logo.png",
+    resources: ['catalog', 'stream', 'meta'],
+    types: ['movie', 'series'],
+    catalogs: [
+        {
+            type: "movie",
+            id: "hellspy",
+            name: "Hellspy videos",
+            extra: [
+                //{ name: "search", isRequired: false }  // ← toto je dôležité
+                { name: "search"}  // ← toto je dôležité
+            ]
+        }
+    ],
+    idPrefixes: ['tt', "hellspy"]
+}
+)
+
+// takes function(args)
+builder.defineStreamHandler(async function(args) {
+    //console.log("!!!!!defineStreamHandler: ", args)
+    let streams = []
+    if(args.id.startsWith("tt"))
+    {
+        id = args.id.split(":")[0]
+        url = `https://api.themoviedb.org/3/find/${id}?api_key=a07324c669cac4d96789197134ce272b&external_source=imdb_id&language=sk-SK,cs-CS&append_to_response=credits,images,release_dates,videos`
+        response = await axios.get(url)
+        query = response.data?.movie_results[0] ? `${response.data.movie_results[0].original_title} ${response.data.movie_results[0].release_date.substring(0, 4)}` : `${response.data.tv_results[0].original_name} s${args.id.split(":")[1]}e${args.id.split(":")[2]}` 
+        //query = response.data?.movie_results[0] ? `${response.data.movie_results[0].title} ${response.data.movie_results[0].release_date.substring(0, 4)}` : `${response.data.tv_results[0].original_name} s${args.id.split(":")[1]}e${args.id.split(":")[2]}` 
+        files = await search(query, false)
+        streams = files.map(file => ({
+            name:  `${file.resolution ? `📺${file.resolution.match(/x(\d+)/i)[1]}p  ` : "" }${file.size ? `💾${file.size}   ` : ""}`, //|| file.name,               // napr. 1080p, fallback na názov
+            url: file.d_link,                                 // link na prehratie
+            description: `${file.name.replaceAll(/ /g, "\u00A0").replace(/\s+(?=\S*$)/, ".")}\n${file.duration ? "⏱" + file.duration : ""}\n${file.audio?.audio_stopy ? "🔊" + file.audio.audio_stopy.split(/\s+/).join("|").replaceAll(",","")  : ""}\n${file.video?.titulky ? "💬" + file.video.titulky.split(/\s+/).join("|").replaceAll(",","") : "" }`,
+            thumbnail: file.thumbnail,
+            //type: "movie"
+            }))       
+        
+    }
+    else //prehranie streamu z vyhladavania jednotlivych suborov z katalogu
+    {
+        console.log("args", args)
+        streams = [
+        {
+            //name: "_______"+args.id.split(";")[1],
+            url: args.id.split(";")[0].replace("hellspy:", ""),
+            // description: "test",
+        }]
+    }
+    //console.log("streams:", streams)
+    return { streams }
+})
+
+builder.defineCatalogHandler(async function(args) {
+    let files = await search(args.extra.search)
+    //console.log("files", files) 
+
+    //console.log("POSTER", files[0].thumbnail) 
+    const metas = files.map(f => ({
+        id: `hellspy:${f.d_link};${f.name};${f.thumbnail};${f.audio?.audio_stopy? `🔊`+f.audio.audio_stopy.replaceAll(",", "|"): ""};${f.video?.titulky? `💬`+f.video.titulky.replaceAll(",", "|"): ""}`,
+        name: f.name,
+        poster: (!f.thumbnail || f.thumbnail.toLowerCase().includes("/images/icons")) ? ("https://fastshare.cloud/" + f.thumbnail) : f.thumbnail,
+        //"posterShape": "regular",
+        description: f.name || "",
+        type: "movie"
+    }))
+
+        // const metas = {
+        // id: `hellspy:TESTTTT`,
+        // name: "TESTTTT2222",
+        // poster: "POSTERRR",
+        // //"posterShape": "regular",
+        // description: "test description",
+        // type: "movie"
+    //}
+    //console.log("METAS:", metas)
+    return { "metas": metas }
+})
+
+builder.defineMetaHandler(function(args) {
+    // console.log("defineMetaHandler_args:", args)
+    // console.log("defineMetaHandler_args:", args.id)
+
+    const metaObj = {
+        id: args.id,
+        name: args.id.split(";")[1],
+        description: `${args.id.split(";")[3]}\n${args.id.split(";")[4]}`,
+        //releaseInfo: '2010',
+        background: args.id.split(";")[2],
+        poster: args.id.split(";")[2],
+        posterShape: 'poster',
+        //type: 'movie'
+    }
+    return Promise.resolve({ meta: metaObj })
+})
+
+async function search(query, video_details = false)
+{
+    console.log("query: ", query)
+    url = `https://api.hellspy.to/gw/search?query=${query.replaceAll(" ", "%20")}}&offset=0&limit=99`
+    response = await axios.get(url)
+            if (response.status == 200)
+            {
+                videos = response.data.items
+            }
+
+    const start = Date.now()
+    let files = []
+    if (videos.length != 0) {
+        for (const video of videos) {
+            let audio = []
+            audio.audio_stopy = ""
+            video_title_lower = video.title.toLowerCase()
+            if (video_title_lower.includes("sk") && !video_title_lower.includes("česk") && !video_title_lower.includes("cesk")) 
+                audio.audio_stopy = "SK"
+		    if (video_title_lower.includes("cz") || video_title_lower.includes("cs") || video_title_lower.includes("český") || video_title_lower.includes("cesky")) 
+                audio.audio_stopy = audio.audio_stopy? audio.audio_stopy + " CZ" : "CZ"
+
+            objectType = video.objectType.toLowerCase()
+            let file = {
+                name: video.title,
+                size: `${(video.size / (1024 ** 3)).toFixed(2)} GB`,
+                duration: `${Math.floor(video.duration / 3600)}h ${Math.floor((video.duration % 3600) / 60)}m`,
+                //resolution: resolution,
+                thumbnail: video.thumbs[0],
+                d_link: `https://api.hellspy.to/${objectType.substring(0, 2)}/${objectType.slice(-5)}/${video.id}/${video.fileHash}/download`,
+                audio: audio
+            }
+            files.push(file)
+        }
+    }
+
+    return files
+}
+
+// Najskôr login, potom spustenie servera
+async function init() {
+    const server = serveHTTP(builder.getInterface(), { port: process.env.PORT || 7001 })
+}
+init()
+
+//serveHTTP(builder.getInterface(), { port: process.env.PORT || 7000 })
+//publishToCentral("https://your-domain/manifest.json") // <- invoke this if you want to publish your addon and it's accessible publically on "your-domain"

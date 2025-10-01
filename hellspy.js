bd0df7db72e87467c1c24175868e39f596028b8e
lerrel129 <45817340+lerrel129@users.noreply.github.com>
2025-10-01 14:40:15 +0200

    Update hellspy.js

--- hellspy.js
+++ hellspy.js
@@ -145,6 +145,16 @@ async function search(query, video_details = false)
 const addonInterface = builder.getInterface();
 
 module.exports = (req, res) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+    if (req.method === "OPTIONS") {
+        res.statusCode = 200;
+        res.end();
+        return;
+    }
+
     if (req.url === "/manifest.json") {
         res.setHeader("Content-Type", "application/json");
         res.end(JSON.stringify(addonInterface.manifest));
@@ -153,11 +163,12 @@ module.exports = (req, res) => {
         return addonInterface.get(req, res);
     }
     else {
-        // všetko ostatné (/, /favicon.ico, /favicon.png, …)
         res.statusCode = 404;
         res.end("Not found");
     }
-}
+};
+
+
 
 
 
